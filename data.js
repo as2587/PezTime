5da14989676ede6903f67e86b89140eb0637d7fd
Abhishek Sriraman <as2587@cornell.edu>
2015-08-07 09:27:25 -0400

    add today date

--- data.js
+++ data.js
@@ -3,36 +3,37 @@ function GetModels(){
  return items;
 }
 
-function GetTimeline(currModel) {
+function GetTimeline(currModel, todaystr) {
 	var items;
 	switch(currModel){
 		case "Chewbacca":
 			items = new vis.DataSet([
-	      	  {id: 2, content: 'item 2', start: '2014-04-14'},
+	      	  {id: 1, content: 'Example', start: '2015-04-12', end: '2015-08-10'},
 
 	    	]);
 			break;
 		case "C3PO":
 			items = new vis.DataSet([
-	      	  {id: 2, content: 'item 2', start: '2014-04-14'},
-
+	      	  {id: 1, content: 'Example', start: '2015-04-24', end: '2015-08-10'},
+			  {id: 2, content: 'Example2', start: '2015-04-22', end: '2015-04-24'},
 	    	]);
 			break;
 		case "Daphne":
 			items = new vis.DataSet([
-		      {id: 1, content: 'Abhishek', start: '2015-07-15', end: '2015-08-10'},
-		      {id: 2, content: 'item 2', start: '2014-04-14'},
+		      {id: 1, content: 'Abhishek', start: '2015-07-15', end: '2015-08-03'},
+			  {id: 2, content: 'Manan', start: '2015-08-03', end: todaystr}
 
 		    ]);
+			break;
 		case "Fred":
 			items = new vis.DataSet([
-		      {id: 2, content: 'item 2', start: '2014-04-14'},
+	      	  {id: 1, content: 'Example', start: '2015-04-14', end: '2015-08-10'},
 
 		    ]);
 			break;
 		case "Gimli":
 			items = new vis.DataSet([
-		      {id: 2, content: 'item 2', start: '2014-04-14'},
+	      	  {id: 1, content: 'Example', start: '2015-04-14', end: '2015-08-10'},
 
 		    ]);
 			break;
